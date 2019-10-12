65e9855796dede14fe28a046b3e175248da7777d
rayblan06 <51677651+rayblan06@users.noreply.github.com>
2019-10-12 17:58:22 +0200

    Add files via upload

---
+++ Descomposicion5d.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 12 17:53:37 2019
+
+@author: Raul Blanco Jimenez
+"""
+
+import sys
+if len(sys.argv) == 2:
+    numero = int(sys.argv[1])
+    if numero < 0 or numero > 99999:
+        print("Introduzca un número entero")
+        print("Debe introducir: Ejercicio3_8.py del 0 al 99999")
+    else: # Esto es la parte importante
+        cadena = str(numero) # Convertimos el número en cadena para saber su longitud
+        longitud = len (cadena)
+        
+        for i in range(longitud):
+            print(" {:05d} ".format( int(cadena[longitud-1-i])*10**i))
+
+
+
+
+
+else:
+    print("Introduzca un número entero")
+    print("Debe introducir: Ejercicio3_8.py del 0 al 99999")
